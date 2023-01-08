618c1536346433f80193dca390c3b3ef67049510
cleverAkanimoh <crushclever1@gmail.com>
2023-01-08 13:22:58 +0100

    modified 3 files in .

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
 const logos = document.querySelectorAll('.logo');
 const alert = document.querySelector('.alert');
+const nav = document.querySelector('nav');
+const textarea = document.getElementById('essay');
+const wordCountDisplay = document.getElementById('count');
+const hearAbout = document.getElementById('information');
+
+function NavFixed() {
+    const height = nav.clientHeight;
+    const scrollHeight = nav.getBoundingClientRect().top
+
+    if (scrollHeight === 0) {
+        nav.classList.remove("nav-fixed")
+    } else {
+        nav.classList.add("nav-fixed")
+    }
+}
+window.onscroll = NavFixed;
 
 function displayAlert(txt) {
     alert.textContent = txt;
+    alert.classList.add("alert-display")
     setTimeout(() => {
-        alert.classList.add("alert-display")
-    }, 5000)
+        alert.textContent = "";
+        alert.classList.remove("alert-display")
+    }, 7000)
 }
 
 // logo refresh event
-logos.forEach(logo => logo.onclick = () =>  window.location.reload())
+logos.forEach(logo => logo.onclick = () => window.location.reload())
 
 // handling form submission
 
@@ -18,12 +36,30 @@ const inputFields = document.querySelectorAll('.form-div input')
 console.log(inputFields);
 
 function HandleSumbit(e) {
-inputFields.forEach(field => {
-    if (field.value === "") {
-        e.preventDefault();
-        displayAlert('please ensure all input fields are filled')
-    }
-})
+    inputFields.forEach(field => {
+        if (field.value === "") {
+            e.preventDefault();
+            displayAlert('please ensure all input fields are filled')
+        }
+    })
 }
+registrationForm.onsubmit = HandleSumbit;
 
-registrationForm.onsubmit = HandleSumbit
+function WordCount() {
+    let textareaValue = textarea.value.trim().split(" ");
+    let wordLength = textareaValue.length;
+    let wordLengthDiff = 150 - textareaValue.length;
+    wordCountDisplay.innerHTML = `you have ${wordLengthDiff} ${wordLength === 1 ? 'word' : "words"} left`;
+    if (wordLengthDiff === 0) {
+        textarea.disabled = true;
+        hearAbout.focus();
+    } else if (wordLength < 10) {
+        textarea.focus()
+        wordCountDisplay.textContent = 'Please enter atleast 10 words';
+    } else if (textareaValue[0] == "") {
+        wordCountDisplay.textContent = "";
+        wordLength = 0;
+    }
+    console.log(textareaValue[0]);
+}
+textarea.oninput = WordCount;
