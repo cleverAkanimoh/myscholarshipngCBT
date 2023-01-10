bf70bf23daa363ad00e9500de0f0cf9b3030ed9e
cleverAkanimoh <crushclever1@gmail.com>
2023-01-10 12:39:10 +0100

    modified App.js deleted CountDown.js

--- App.js
+++ App.js
@@ -27,3 +27,50 @@ function modulesLoad() {
     });
 }
 window.addEventListener('DOMContentLoaded', modulesLoad)
+
+// countdown
+
+let d = new Date(2023, 2, 11, 14, 0);
+const year = d.getFullYear();
+const month = d.getMonth();
+const day = d.getDay();
+const date = d.getDate();
+const hour = d.getHours();
+const min = d.getMinutes();
+
+holding.textContent = `the computer based test will hold on ${Weekdays[day]}, ${date} ${Months[month]} ${year}, ${hour}:${min}${min > 9 ? "" : "0"}`;
+
+// future time in milliseconds
+
+let futureTime = d.getTime();
+
+function getRemainingTime() {
+    const today = new Date().getTime();
+    const timeDiffBtweenNowAndFuture = futureTime - today;
+    // 1s = 1000ms
+    // 1m = 60s
+    // 1hr = 60mins
+    // 1d = 24hr
+
+    // values in ms
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+
+    // calculate  values 
+    let daysLeft = Math.floor(timeDiffBtweenNowAndFuture / oneDay);
+    let hoursLeft = Math.floor((timeDiffBtweenNowAndFuture % oneDay) / oneHour);
+    let minutesLeft = Math.floor((timeDiffBtweenNowAndFuture % oneHour) / oneMinute);
+    let secondsLeft = Math.floor((timeDiffBtweenNowAndFuture % oneMinute) / 1000);
+
+    // display values in items 
+
+    countdown.textContent = `the scholarship test will commence in ${daysLeft > 9 ? daysLeft : '0' + daysLeft}${daysLeft ? 'days' : "day"} ${hoursLeft > 9 ? hoursLeft : '0' + hoursLeft}${hoursLeft ? 'hours' : "hour"} ${minutesLeft > 9 ? minutesLeft : '0' + minutesLeft}${minutesLeft ? 'minutes' : "minute"} and ${secondsLeft > 9 ? secondsLeft : '0' + secondsLeft} ${secondsLeft ? 'seconds' : "second"}`;
+
+    if (timeDiffBtweenNowAndFuture < 0) {
+        clearInterval(interval);
+        deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired`;
+        giveaway.textContent = `giveaway ended on ${Weekdays[day]}, ${date} ${Months[month]} ${year}, ${hour}:${min}am`;
+    }
+};
+let interval = setInterval(getRemainingTime, 1000);
