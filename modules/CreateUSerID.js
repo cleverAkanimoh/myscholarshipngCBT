3a12c986188bb0db337523084d2ce083b35e3073
cleverAkanimoh <crushclever1@gmail.com>
2023-01-10 08:14:51 +0100

    modified CreateUSerID.js in modules

--- modules/CreateUSerID.js
+++ modules/CreateUSerID.js
@@ -1,9 +1,9 @@
-let uniqueNumber = Date.now()
 
 export default function(){
+    let uniqueNumber = Date.now()
     let randomNumber = Math.floor(Math.random() * 10)
     if (firstName.value === ""){
         userID.value = '';
     }
-    userID.value = `${firstName.value}-${randomNumber}${uniqueNumber}`
+    userID.value = `${firstName.value}${randomNumber}${uniqueNumber}`
 }
